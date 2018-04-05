fdb94d35946527a4d3f2e21a1a56505c0802410e
joshwilkins2013 <joshwilkins2013@gmail.com>
2018-04-05 13:55:21 -0400

    Started car make/model analysis and added new best fit method to Analyzer

--- lib/Analyzer.py
+++ lib/Analyzer.py
@@ -5,83 +5,158 @@ import pandas as pd
 from itertools import groupby
 from matplotlib import pyplot as plt
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline
+
 
 class Analyzer(object):
-	
+
 	def __init__(self):
+		self.data = {}
 		self.merge_results()
-	
+		self.items = self.data.keys()
+
 	def _clean_results(self):
 		""" Clean up some of the columns to be consistent """
-		if self.file_type == "Automobile":
-			self.data.Mileage.replace([',', 'mi.', 'nan', ' '], '', regex=True, inplace = True)  # Fix mileage column
-		else:
-			self.data.Area = self.data.Area.map(lambda x: str(x)[:-3])  # Fix square footage column
-		
-		self.data.Price.replace([',', '\$'], '', regex=True, inplace=True)  # Always fix price column (, and $ removed)
-		self.data.replace('^\s*$', np.nan, regex=True, inplace = True)  # Replace all empty values with np.NaN
-		self.data = self.data.dropna(axis=1, how='all')  # Remove Null Columns
-		
-		self.data.apply(pd.to_numeric, errors='coerce') # Coerces errors into NaN values
-	
+		for item, item_data in self.data.iteritems():
+			if self.file_type == "Automobile":
+				cols = ["Year", "Mileage", "Price"]
+				item_data.Mileage.replace([',', 'mi.', 'nan', ' '], '', regex=True, inplace = True)  # Fix mileage column
+				item_data.Price.replace([',', '\$'], '', regex=True, inplace=True)  # Always fix price column (, and $ removed)
+				item_data[cols] = item_data[cols].apply(pd.to_numeric, errors='coerce')  # Coerces errors into NaN values
+				item_data.drop(item_data[item_data.Year < 2000].index, inplace=True) # Remove cars made before 2000
+				item_data.drop(item_data[item_data.Price > 30000].index, inplace=True) # Remove cars over 30,000
+				item_data.drop(item_data[(item_data.Mileage < 1000) | (item_data.Mileage > 300000)].index, inplace=True) # Remove cars with over 300,000 miles
+
+				item_data['Age'] = 2018 - item_data['Year'] # Change years to Age
+				del item_data['Year']
+			else:
+				item_data.Area = item_data.Area.map(lambda x: str(x)[:-3])  # Fix square footage column
+				item_data.Price.replace([',', '\$'], '', regex=True,
+										inplace=True)  # Always fix price column (, and $ removed)
+			item_data.replace('^\s*$', np.nan, regex=True, inplace = True)  # Replace all empty values with np.NaN
+			item_data = item_data.dropna(axis=1, how='all')  # Remove Null Columns
+			item_data = item_data.apply(pd.to_numeric, errors='coerce') # Coerces errors into NaN values
+
 	def get_data(self, dir='.\\Data\\'):
 		""" Gather & sort data by item name in Data folder
 		return: An ordered dictionary {Item : [list of files]}
 		"""
 		# Get list of data files
 		csv_files = glob.glob(dir + "*.csv")
-		
+
 		# Groups list into dictionary-like object by SN #
 		item_groups = groupby(csv_files, lambda x: x[:x.find('_')])
 		item_groups = dict( (item, list(files)) for (item, files) in item_groups )
-		
+
 		return item_groups
-	
+
 	def merge_results(self):
-		self.data = pd.DataFrame()
 		item_groups = self.get_data()
-		
+
 		for item, file_group in item_groups.iteritems():
 			item_data = pd.DataFrame()
 			for file_name in file_group:
 				df = pd.read_csv(file_name)
 				item_data = item_data.append(df)
-				
+
 			item_data.to_csv(item + "_Merged.csv", index=False)
-			self.data = self.data.append(item_data)
-			
+			self.data[item[7:]] = item_data
+
 			# Now that we know the data is safely stored, remove the old stuff
 			for file_name in file_group:
+				if "Merged" in file_name: continue
 				os.remove(file_name)
-		
-		if 'Mileage' in self.data.columns:
+
+		if 'Mileage' in item_data.columns:
 			self.file_type = "Automobile"
 		else:
 			self.file_type = "Apartment"
-		
-		self._clean_results()
-		self.data.to_csv("MergedData.csv", index=False)
 
-	def plot_results(self):
-	
+		self._clean_results()
+
+	def _poly_fit(self, degree=2, **kwargs):
+		return make_pipeline(PolynomialFeatures(degree), LinearRegression(**kwargs))
+
+	def best_fit(self, item, col='Age', verbose=False):
+
+		param_grid = {'polynomialfeatures__degree': np.arange(2, 7, 1),
+					  'linearregression__fit_intercept': [True, False],
+					  'linearregression__normalize': [True, False]}
+
+		if col == 'Age':
+			group = self.data[item].groupby(self.data[item][col])[['Price']].mean().reset_index()[col].values
+			mean_costs = self.data[item].groupby(self.data[item][col])[['Price']].mean().reset_index()['Price'].values
+			median_costs = self.data[item].groupby(self.data[item][col])[['Price']].median().reset_index()['Price'].values
+			metric = ((mean_costs + median_costs)/2)
+
+			X_test = np.linspace(group[0], group[-1], len(group)+2)[:, None]
+			X = group.reshape(len(group),1)
+			y = metric
+		else:
+			temp_df = self.data[item][[col, "Price"]].dropna(axis=0, how='any')  # Remove rows with missing values
+			X = temp_df[col].values.reshape(len(temp_df),1)
+			y = temp_df["Price"].values
+			X_test = np.linspace(min(temp_df[col]), max(temp_df[col]), 1000)[:, None]
+
+		grid = GridSearchCV(self._poly_fit(), param_grid)
+		grid.fit(X, y)
+
+		model = grid.best_estimator_
+
+		plt.subplot(1,2,1)
+		if verbose:
+			plt.scatter(X.ravel(), y)
+
+		y_test = model.fit(X, y).predict(X_test)
+		plt.plot(X_test.ravel(), y_test, label=item)
+
+		plt.xlim(xmin=0)
+		plt.ylim(ymin=0)
+		plt.title(col + ' vs Cost')
+		plt.grid(which='both')
+		plt.legend()
+
+		# Plot its derivative too - Shows depreciation rate better
+		plt.subplot(1,2,2)
+		best_order = grid.best_params_["polynomialfeatures__degree"]
+		coeffs = np.polyfit(X.ravel(), y, best_order)
+
+		p = np.poly1d(np.negative(coeffs))
+		if col == 'Mileage':
+			p *= 10000 # Convert derivative to $ per 10,000 miles instead of $ per mile
+
+		p2 = np.polyder(p)
+		plt.plot(X_test.ravel(), p2(X_test.ravel()), label=item)
+
+		plt.xlim(xmin=0)
+		plt.ylim(ymin=0)
+		plt.title(col + ' vs Cost')
+		plt.grid(which='both')
+		plt.legend()
+
+	def plot_results(self, item):
+
 		fig = plt.figure(figsize=(13,6))
-		
+
 		if self.file_type == "Automobile":
-			cols = ['Mileage', 'Price', 'Year', 'Link']
+			cols = ['Mileage', 'Price', 'Age', 'Link']
 			plt.xlabel('Mileage')
-			plt.title('Mileage vs Cost (Year in Color)')
+			plt.title('Mileage vs Cost (Age in Color)')
 		else:
 			cols = ['Area', 'Price', 'Bedrooms', 'Link']
 			plt.xlabel('Square Feet')
 			plt.title('SqFt vs Cost (Number of Bedrooms in Color)')
-		
-		new_df = self.data[cols]
+
+		new_df = self.data[item][cols]
 		new_df = new_df.dropna(axis=0, how='any')  # Remove rows with missing values
-		
+
 		for col in cols[:-1]:
 			new_df[col] = new_df[col].astype(int)
-		
-		s = plt.scatter(x=new_df[cols[0]], y=new_df[cols[1]], c=new_df[cols[2]], cmap='plasma')
+
+		s = plt.scatter(x=new_df[cols[0]], y=new_df[cols[1]], c=new_df[cols[2]], cmap='plasma_r')
 		s.set_urls(new_df['Link'].values)
 		plt.colorbar()
 		plt.grid(which='both')
